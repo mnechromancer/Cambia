2a58c9835a0f49770296b451ba7ae0c2b4310992
JUMPducey <jamison.ducey@cognixia.com>
2024-05-08 16:26:17 -0400

    Continuous neighbors and incremental updates (from discrete neighbors and conditional updates)

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
+import scipy.signal as signal
 import numpy as np
 
 # Initialize PyGame
 pygame.init()
 
 # Set up display
-size = 512
-cell_size = 1
+size = 256
+cell_size = 2
 width, height = size * cell_size, size * cell_size
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Conway\'s Game of Life')
@@ -14,21 +15,30 @@ pygame.display.set_caption('Conway\'s Game of Life')
 # Initialize grid
 np.random.seed(0)
 A = np.random.randint(2, size=(size, size))
+K = np.asarray([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+K_sum = np.sum(K)
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+def growth(U):
+    return 0 + (U==3) - ((U<2) | (U>3))
+
 def update(A):
-    # Neighbor sum
-    U = sum(np.roll(A, (i, j), axis=(0, 1)) for i in (-1, 0, 1) for j in (-1, 0, 1) if (i != 0 or j != 0))
-    # Conditional update
-    return (A & (U == 2)) | (U == 3)
+    # Neighbor sum (for discrete neighbors)
+    # U = sum(np.roll(A, (i, j), axis=(0, 1)) for i in (-1, 0, 1) for j in (-1, 0, 1) if (i != 0 or j != 0))
+    # Convolution (for continuous neighbors)
+    U = signal.convolve2d(A, K, mode='same', boundary='wrap')
+    # Conditional Update
+    # return (A & (U==2) | (U==3))
+    # Incremental Update and Clipping
+    return np.clip(A + growth(U), 0, 1)
 
 def draw_grid(screen, A):
     for y in range(size):
         for x in range(size):
-            color = WHITE if A[y, x] == 1 else BLACK
+            color = BLACK if A[y, x] == 1 else WHITE
             pygame.draw.rect(screen, color, (x * cell_size, y * cell_size, cell_size, cell_size))
 
 # Main loop
